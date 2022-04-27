5394b120f604c9f91479782059bc19029a235c4d
Gustavo <gordono@unsa.edu.pe>
2022-04-27 09:05:00 -0500

    Configuración de la Base de Datos

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { port } = require("./config");
+const { connection } = require("./config/db");
+
+connection();
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Listening on: http://localhost:${port}`);
+});
